afbb76fec6677c3d71864b695af1dc200b155ea7
JohnMabas <johnmabas1@gmail.com>
2024-09-28 21:35:45 +0100

    make an update

--- src/components/Sreach.jsx
+++ src/components/Sreach.jsx
@@ -7,7 +7,71 @@ const Sreach = () => {
         <input className='w-2/4 h-12 rounded-full border-solid border-2 border-black text-center ' type="text" placeholder='Sreach'></input>
       </div>
       <hr className='mt-5 h-10'></hr>
+
+      <div className='grid grid-cols-4 gap-4 m-10 p-2'>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/4f7a276e-9995-4b32-bda6-300de0619b25.png?im_w=1440&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Stay in Prince’s Purple Rain house</a></p>
+            <p>Hosted by Wendy and Lisa</p>
+            <p className='font-medium'>Coming September</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNjMyMzc5Mzc2MTc3OTEzMg%3D%3D/original/01819c16-43a1-4aee-9957-9edce6eb8e16.png?im_w=1200&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Join a living room session with Doja</a></p>
+            <p>Hosted by Doja Cat</p>
+            <p className='font-medium'>Coming October</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE3NzY2MTYzNDg4MjE2ODY1Nw%3D%3D/original/a332d020-4315-4f63-af71-444d46474939.png?im_w=1440&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Sleepover at Polly Pocket’s Compact</a></p>
+            <p>Hosted by Polly Pocket</p>
+            <p className='font-medium'>Sold Out</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE4NzE3Nzg1NDA2MjM5NzY2NQ%3D%3D/original/6989d581-3f67-4cd9-8cb6-5f5c226aedc6.png?im_w=1440&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold' ><a href='#'>Playdate at Polly Pocket’s Compact</a></p>
+            <p>Hosted by Polly Pocket</p>
+            <p className='font-medium'>Sold Out</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4NzY0ODgzNzUzNjQzNw%3D%3D/original/1077cfcd-29d5-42b7-adab-19e0b620e492.jpeg?im_w=1440&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Go VIP with Kevin Hart</a></p>
+            <p>Hosted by Keven Hart</p>
+            <p className='font-medium'>Sold Out</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNjIzMTk3NDU3MjE4Nzg2NA%3D%3D/original/f4cbe542-3ce0-4c6f-a8f1-d2120c1b2420.jpeg?im_w=1440&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Train at the X-Mansion</a></p>
+            <p>Hosted by Jubilee</p>
+            <p className='font-medium'>Sold Out</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4NjI3OTI1MjIxNDQyOA%3D%3D/original/bc989f2d-eca8-4bcf-a9b0-b70b8e685a64.jpeg?im_w=960&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Live like Bollywood star Janhvi Kapoor</a></p>
+            <p>Hosted by Janhvi Kapoor</p>
+            <p className='font-medium'>Sold Out</p>
+        </div>
+
+        <div>
+            <img className='w-64 h-64 rounded-2xl' src='https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE2MjI1MjI0NDQ0MzYzMjM4Mg%3D%3D/original/ae3426d1-fba4-44d4-bed2-690426f25f7a.jpeg?im_w=960&im_q=highq' alt=''/>
+            <p className='pt-2 font-bold'><a href='#'>Open the Olympic Games at Musée d’Orsay</a></p>
+            <p>Hosted by Mathieu Lehnneur</p>
+            <p className='font-medium'>Sold Out</p>
+        </div>
+        
+
+
+      </div>
     </div>
+
+    
   )
 }
 
